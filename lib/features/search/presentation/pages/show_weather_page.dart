a5bf91884f2ab3e25d083ba95d3ecaed9e644e9a
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-01 19:36:02 -0600

    feat: single scroll updated

--- lib/features/search/presentation/pages/show_weather_page.dart
+++ lib/features/search/presentation/pages/show_weather_page.dart
@@ -12,11 +12,20 @@ class _ShowWeatherPageState extends State<ShowWeatherPage> {
   Widget build(BuildContext context) {
 
     final forecastData = [
-      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s'},
-      {'hour': '16:00', 'icon': Icons.cloud, 'rain': '37%', 'wind': '7 m/s'},
-      {'hour': '17:00', 'icon': Icons.wb_sunny, 'rain': '23%', 'wind': '4 m/s'},
-      {'hour': '18:00', 'icon': Icons.cloud, 'rain': '45%', 'wind': '6 m/s'},
-      {'hour': '19:00', 'icon': Icons.cloudy_snowing, 'rain': '75%', 'wind': '7 m/s'},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': 'Now', 'icon': Icons.wb_sunny, 'rain': '14%', 'wind': '4 m/s', 'temp' : 36.4},
+      {'hour': '16:00', 'icon': Icons.cloud, 'rain': '37%', 'wind': '7 m/s', 'temp' : 38.1},
+      {'hour': '17:00', 'icon': Icons.wb_sunny, 'rain': '23%', 'wind': '4 m/s', 'temp' : 29.6},
+      {'hour': '18:00', 'icon': Icons.cloud, 'rain': '45%', 'wind': '6 m/s', 'temp' : 29.6},
+      {'hour': '19:00', 'icon': Icons.cloudy_snowing, 'rain': '75%', 'wind': '7 m/s', 'temp' : 30.6},
     ];
 
     return Scaffold(
@@ -68,15 +77,15 @@ class _ShowWeatherPageState extends State<ShowWeatherPage> {
                             fontSize: 16
                           )
                         ),
-                        Row(
+                        const Row(
                           children: [
-                            const Icon(
+                            Icon(
                               Icons.sunny,
                               color: Colors.amber,
                               size: 70,
                             ),
-                            const SizedBox(width: 15),
-                            const Expanded(
+                            SizedBox(width: 15),
+                            Expanded(
                               flex: 1,
                               child: Text(
                                 "26 °C",
@@ -85,70 +94,98 @@ class _ShowWeatherPageState extends State<ShowWeatherPage> {
                                   color: Colors.white
                                 ),
                               ),
-                            ),
-                            Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Text(
-                                  '26°',
-                                  style: TextStyle(
-                                    fontSize: 28,
-                                    fontWeight: FontWeight.w500,
-                                      color: Colors.white
-                                  ),
-                                ),
-                                Container(
-                                  height: 2,
-                                  width: 40,
-                                  color: Colors.white.withOpacity(0.6),
-                                ),
-                                const Text(
-                                  '16°',
-                                  style: TextStyle(
-                                    fontSize: 28,
-                                    fontWeight: FontWeight.w400,
-                                    color: Colors.white
-                                  ),
-                                ),
-                              ],
                             )
                           ],
                         ),
                         Container(
-                          padding: const EdgeInsets.all(12),
+                          padding: const EdgeInsets.all(20),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(24),
                             color: Colors.white
                           ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              ...forecastData.map((data) {
-                                return Column(
-                                  children: [
-                                    Text("${data['hour']}", style: const TextStyle(fontWeight: FontWeight.bold)),
-                                    const SizedBox(height: 4),
-                                    Icon(data['icon'] as IconData, size: 28, color: Colors.blueAccent),
-                                    const SizedBox(height: 4),
-                                    Column(
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                ...forecastData.map((data) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(right: 18),
+                                    child: Column(
                                       children: [
-                                        const Icon(Icons.water_drop_outlined, size: 16, color: Colors.blue),
-                                        Text("${data['rain']}", style: const TextStyle(fontSize: 12)),
+                                        Text("${data['hour']}", style: const TextStyle(fontWeight: FontWeight.bold)),
+                                        const SizedBox(height: 12),
+                                        Icon(data['icon'] as IconData, size: 28, color: Colors.blueAccent),
+                                        const SizedBox(height: 12),
+                                        Column(
+                                          children: [
+                                            const Icon(Icons.water_drop_outlined, size: 16, color: Colors.blue),
+                                            Text("${data['rain']}", style: const TextStyle(fontSize: 12)),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 12),
+                                        Column(
+                                          children: [
+                                            const Icon(Icons.air, size: 16, color: Colors.blueGrey),
+                                            Text("${data['wind']}", style: const TextStyle(fontSize: 12)),
+                                          ],
+                                        ),
                                       ],
                                     ),
-                                    const SizedBox(height: 4),
-                                    Column(
-                                      children: [
-                                        const Icon(Icons.air, size: 16, color: Colors.blueGrey),
-                                        Text("${data['wind']}", style: const TextStyle(fontSize: 12)),
-                                      ],
-                                    ),
-                                  ],
-                                );
-                              })
-                            ],
+                                  );
+                                })
+                              ],
+                            ),
                           ),
-                        )
+                        ),
+                        const SizedBox(height: 24),
+                        Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(24),
+                              color: Colors.white
+                            ),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  ...forecastData.map((data) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(vertical: 8),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Expanded(child: Text("${data['hour']}", style: const TextStyle(fontWeight: FontWeight.bold))),
+                                          Expanded(
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                const Icon(Icons.water_drop_outlined, size: 16, color: Colors.blue),
+                                                Text("${data['rain']}", style: const TextStyle(fontSize: 12)),
+                                              ],
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                const Icon(Icons.air, size: 16, color: Colors.blueGrey),
+                                                Text("${data['wind']}", style: const TextStyle(fontSize: 12)),
+                                              ],
+                                            ),
+                                          ),
+                                          Expanded(child: Icon(data['icon'] as IconData, size: 28, color: Colors.blueAccent)),
+                                          Expanded(child: Text("${data['temp']}°C")),
+                                        ],
+                                      ),
+                                    );
+                                  }),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10,)
                       ],
                     ),
                   ),
