fac2bb43e734169f0f6e8f4c8b765c158dfb0314
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-09 14:31:35 -0600

    feat: background color updated

--- lib/features/search/presentation/blocs/cubits/background_weather_cubit.dart
+++ lib/features/search/presentation/blocs/cubits/background_weather_cubit.dart
@@ -1,11 +1,25 @@
+import 'dart:async';
+
+import 'package:clima_app/core/colors/weather_colors.dart';
+import 'package:clima_app/features/search/domain/entities/translated/translated_weather.dart';
+import 'package:clima_app/features/search/presentation/blocs/weather_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BackgroundWeatherCubit extends Cubit<Color?>{
 
-  BackgroundWeatherCubit() : super(Colors.blue[300]);
+  final WeatherCubit _weatherCubit;
+  late final StreamSubscription weatherSubscription;
 
-  void setBackgroundColor(Color color) {
-    emit(color);
+  BackgroundWeatherCubit(this._weatherCubit) : super(Colors.blue[300]) {
+    weatherSubscription = _weatherCubit.stream.listen((weatherState) {
+      _mapWeatherToTheme(weatherState.translatedWeather);
+    });
+  }
+
+  void _mapWeatherToTheme(TranslatedWeather? translatedWeather) {
+    if (translatedWeather == null) return;
+
+    emit(WeatherColors.currentWeatherColor(translatedWeather.main));
   }
 }
