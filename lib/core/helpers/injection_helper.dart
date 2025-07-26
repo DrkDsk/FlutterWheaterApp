648daa86a43a01c2066e1939fb4f4217263ef29a
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-25 19:33:33 -0600

    feat: injector modified

---
+++ lib/core/helpers/injection_helper.dart
@@ -0,0 +1,90 @@
+import 'package:clima_app/features/city/data/datasources/city_datasource.dart';
+import 'package:clima_app/features/city/data/repositories/city_repository_impl.dart';
+import 'package:clima_app/features/city/domain/repositories/city_repository.dart';
+import 'package:clima_app/features/city/domain/usecases/get_city_usecase.dart';
+import 'package:clima_app/features/city/domain/usecases/search_city_usecase.dart';
+import 'package:clima_app/features/city/infrastructure/datasources/city_datasource_impl.dart';
+import 'package:clima_app/features/city/presentation/blocs/city_bloc.dart';
+import 'package:clima_app/features/favorites/data/datasources/favorite_weather_datasource.dart';
+import 'package:clima_app/features/favorites/data/repositories/favorite_weather_repository_impl.dart';
+import 'package:clima_app/features/favorites/domain/repository/favorite_weather_repository.dart';
+import 'package:clima_app/features/favorites/infrastructure/datasources/favorite_weather_datasource_impl.dart';
+import 'package:clima_app/features/favorites/presentation/blocs/favorite_bloc.dart';
+import 'package:clima_app/features/home/data/datasources/search_weather_datasource.dart';
+import 'package:clima_app/features/home/data/repositories/location_repository_impl.dart';
+import 'package:clima_app/features/home/data/repositories/search_weather_repository_impl.dart';
+import 'package:clima_app/features/home/data/repositories/weather_description_repository_impl.dart';
+import 'package:clima_app/features/home/domain/repositories/location_repository.dart';
+import 'package:clima_app/features/home/domain/repositories/search_weather_repository.dart';
+import 'package:clima_app/features/home/domain/repositories/weather_description_repository.dart';
+import 'package:clima_app/features/home/domain/services/location_service.dart';
+import 'package:clima_app/features/home/domain/usecases/get_weather_use_case.dart';
+import 'package:clima_app/features/home/infrastructure/datasources/location_datasource_impl.dart';
+import 'package:clima_app/features/home/infrastructure/datasources/search_weather_datasource_impl.dart';
+import 'package:clima_app/features/home/infrastructure/datasources/weather_description_local_datasource_impl.dart';
+import 'package:clima_app/features/home/presentation/blocs/cubits/background_weather_cubit.dart';
+import 'package:clima_app/features/home/presentation/blocs/cubits/theme_cubit.dart';
+import 'package:clima_app/features/home/presentation/blocs/weather_bloc.dart';
+import 'package:clima_app/features/home/presentation/dto/weather_mapper.dart';
+import 'package:get_it/get_it.dart';
+import 'package:clima_app/core/dio_client.dart';
+import 'package:clima_app/core/helpers/hive_initializer.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> initDependencies() async {
+  // Core
+  final dioClient = DioClient();
+  final box = await HiveInitializer.init();
+  getIt.registerSingleton(dioClient);
+  getIt.registerSingleton(box);
+
+  // DataSources
+  getIt.registerLazySingleton<CityDataSource>(() => CityDataSourceImpl(dio: dioClient.dio));
+  getIt.registerLazySingleton<SearchWeatherDataSource>(() => SearchWeatherDatasourceImpl(dio: dioClient.dio));
+  getIt.registerLazySingleton(() => LocationDataSourceImpl());
+  getIt.registerLazySingleton(() => WeatherDescriptionLocalDataSourceImpl());
+  getIt.registerLazySingleton<FavoriteWeatherDataSource>(() => FavoriteWeatherDataSourceImpl(box: box));
+
+  // Repositories
+  getIt.registerLazySingleton<CityRepository>(
+        () => CityRepositoryImpl(dataSource: getIt()),
+  );
+
+  getIt.registerLazySingleton<SearchWeatherRepository>(
+        () => SearchWeatherRepositoryImpl(datasource: getIt()),
+  );
+
+  getIt.registerLazySingleton<FavoriteWeatherRepository>(
+        () => FavoriteWeatherRepositoryImpl(dataSource: getIt()),
+  );
+
+  getIt.registerLazySingleton<WeatherDescriptionRepository>(
+        () => WeatherDescriptionRepositoryImpl(dataSource: getIt()),
+  );
+
+  getIt.registerLazySingleton<LocationRepository>(() => LocationRepositoryImpl(getIt()));
+
+  // Services
+  getIt.registerLazySingleton(() => LocationService(getIt()));
+
+  // UseCases
+  getIt.registerLazySingleton<SearchCityUseCase>(
+        () => SearchCityUseCase(repository: getIt<CityRepository>()),
+  );
+  getIt.registerLazySingleton(() => GetCityUseCase(repository: getIt<CityRepository>()));
+  getIt.registerLazySingleton(() => GetWeatherUseCase(
+    repository: getIt<SearchWeatherRepository>(),
+    locationService: getIt<LocationService>(),
+  ));
+
+  // Helpers
+  getIt.registerLazySingleton(() => WeatherMapper(getIt()));
+
+  // Blocs / Cubits
+  getIt.registerFactory<CityBloc>(() => CityBloc(useCase: getIt<SearchCityUseCase>(), getCityUseCase: getIt<GetCityUseCase>()));
+  getIt.registerFactory<FavoriteBloc>(() => FavoriteBloc(repository: getIt<FavoriteWeatherRepository>()));
+  getIt.registerFactory<WeatherBloc>(() => WeatherBloc(useCase: getIt<GetWeatherUseCase>(), mapper: getIt<WeatherMapper>()));
+  getIt.registerFactory<ThemeCubit>(() => ThemeCubit());
+  getIt.registerFactory<BackgroundWeatherCubit>(() => BackgroundWeatherCubit(getIt<WeatherBloc>()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,29 @@
-import 'package:clima_app/core/dio_client.dart';
-import 'package:clima_app/core/helpers/hive_initializer.dart';
-import 'package:clima_app/features/city/data/repositories/city_repository_impl.dart';
-import 'package:clima_app/features/city/domain/usecases/get_city_usecase.dart';
-import 'package:clima_app/features/city/infrastructure/datasources/city_datasource_impl.dart';
-import 'package:clima_app/features/favorites/data/repositories/favorite_weather_repository_impl.dart';
-import 'package:clima_app/features/favorites/infrastructure/datasources/favorite_weather_datasource_impl.dart';
+import 'package:clima_app/core/helpers/injection_helper.dart';
 import 'package:clima_app/features/favorites/presentation/blocs/favorite_bloc.dart';
-import 'package:clima_app/features/home/infrastructure/datasources/location_datasource_impl.dart';
-import 'package:clima_app/features/home/infrastructure/datasources/search_weather_datasource_impl.dart';
-import 'package:clima_app/features/home/infrastructure/datasources/weather_description_local_datasource_impl.dart';
-import 'package:clima_app/features/home/data/repositories/location_repository_impl.dart';
-import 'package:clima_app/features/home/data/repositories/search_weather_repository_impl.dart';
-import 'package:clima_app/features/home/data/repositories/weather_description_repository_impl.dart';
-import 'package:clima_app/features/home/domain/services/location_service.dart';
-import 'package:clima_app/features/home/domain/usecases/get_weather_use_case.dart';
 import 'package:clima_app/features/home/presentation/blocs/cubits/background_weather_cubit.dart';
-import 'package:clima_app/features/home/presentation/blocs/weather_bloc.dart';
+import 'package:clima_app/features/home/presentation/blocs/cubits/theme_cubit.dart';
 import 'package:clima_app/features/home/presentation/blocs/events/weather_event.dart';
-import 'package:clima_app/features/home/presentation/dto/weather_mapper.dart';
+import 'package:clima_app/features/home/presentation/blocs/weather_bloc.dart';
 import 'package:clima_app/src/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'core/helpers/timezone_config.dart';
-import 'features/home/presentation/blocs/cubits/theme_cubit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   TimeZoneConfig.initTimeZone();
 
-  final dioClient = DioClient();
-  final box = await HiveInitializer.init();
-
-  final repositoriesProviders = [
-    RepositoryProvider(
-      create: (_) => GetCityUseCase(
-          repository: CityRepositoryImpl(
-              dataSource: CityDataSourceImpl(dio: dioClient.dio))),
-    ),
-
-    RepositoryProvider<GetWeatherUseCase>(
-        create: (_) => GetWeatherUseCase(
-            locationService: LocationService(
-                LocationRepositoryImpl(LocationDataSourceImpl())),
-            repository: SearchWeatherRepositoryImpl(
-              datasource: SearchWeatherDatasourceImpl(dio: dioClient.dio),
-            ))),
-
-    RepositoryProvider(
-        create: (_) => WeatherMapper(WeatherDescriptionRepositoryImpl(
-            dataSource: WeatherDescriptionLocalDataSourceImpl())))
-  ];
-
-  final blocsProviders = [
-    BlocProvider(
-        create: (_) => FavoriteBloc(
-            repository: FavoriteWeatherRepositoryImpl(
-                dataSource: FavoriteWeatherDataSourceImpl(box: box)))),
-
-    BlocProvider<WeatherBloc>(
-      create: (context) => WeatherBloc(
-          useCase: context.read<GetWeatherUseCase>(),
-          mapper: context.read<WeatherMapper>())
-        ..add(const CurrentWeatherEvent()),
-    ),
-
-    BlocProvider(create: (_) => ThemeCubit()),
-
-    BlocProvider<BackgroundWeatherCubit>(
-      create: (context) => BackgroundWeatherCubit(context.read<WeatherBloc>()),
-    ),
-  ];
+  await initDependencies();
 
   runApp(
     MultiRepositoryProvider(
-      providers: repositoriesProviders,
-      child: MultiBlocProvider(
-        providers: blocsProviders,
-        child: const MyApp(),
-      ),
+      providers: [
+        BlocProvider(create: (_) => getIt<FavoriteBloc>()),
+        BlocProvider(create: (_) => getIt<WeatherBloc>()..add(const CurrentWeatherEvent())),
+        BlocProvider(create: (_) => getIt<ThemeCubit>()),
+        BlocProvider(create: (_) => getIt<BackgroundWeatherCubit>()),
+      ],
+      child:const MyApp(),
     ),
   );
 }
