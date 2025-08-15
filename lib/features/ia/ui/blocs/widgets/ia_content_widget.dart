bf776ee6d034082126db9aed5cf9cfc557ca6073
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-15 14:54:01 -0600

    feat: card updated

--- lib/features/ia/ui/blocs/widgets/ia_content_widget.dart
+++ lib/features/ia/ui/blocs/widgets/ia_content_widget.dart
@@ -13,38 +13,26 @@ class IAContentWidget extends StatelessWidget {
       borderRadius: BorderRadius.circular(20),
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-        child: SizedBox(
-          height: 300,
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.pinkAccent.withOpacity(0.10),
-                borderRadius: const BorderRadius.vertical(
-                    bottom: Radius.circular(45), top: Radius.circular(45)),
-                border: Border.all(
-                  color: Colors.white.withOpacity(0.3),
-                  width: 1,
-                )),
-            child: Stack(
-              children: [
-                const Positioned(
-                  top: 110,
-                  left: 0,
-                  right: 0,
-                  child: IADescriptionCard(
-                    color: Colors.transparent,
-                  )
-                ),
-                Positioned(
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    child: IADescriptionCard(
-                      isTheFirstOne: true,
-                      color: Colors.deepPurple.shade400.withOpacity(0.20),
-                    )
-                ),
-            ]),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.pinkAccent.withOpacity(0.10),
+              borderRadius: const BorderRadius.vertical(
+                  bottom: Radius.circular(45), top: Radius.circular(45)),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.3),
+                width: 1,
+              )
           ),
+          child: Column(children: [
+            IADescriptionCard(
+              text: "Ideal para ropa ligera pero lleva paraguas por si llueve.",
+              color: Colors.deepPurple.shade200.withOpacity(0.10),
+            ),
+            IADescriptionCard(
+              text: "Hoy hará calor y húmedo, con una ligera brisa",
+              color: Colors.grey.shade400.withOpacity(0.20),
+            ),
+          ]),
         ),
       ),
     );
@@ -52,34 +40,28 @@ class IAContentWidget extends StatelessWidget {
 }
 
 class IADescriptionCard extends StatelessWidget {
-
-  final bool isTheFirstOne;
-
   final Color color;
+  final String text;
 
-  const IADescriptionCard({super.key, required this.color, this.isTheFirstOne = false});
+  const IADescriptionCard(
+      {super.key,
+      required this.color,
+      required this.text});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(left: 24, right: 24, top: 20, bottom: 10),
-      decoration: BoxDecoration(
-          color: color,
-          borderRadius: const BorderRadius.all(Radius.circular(45))),
       child: Padding(
-        padding: isTheFirstOne ? EdgeInsets.zero : const EdgeInsets.only(top: 20.0),
-        child: const Text(
-            "Plants and trees"
-            "Plants and trees"
-            "Plants and trees"
-            "Plants and trees"
-            "Plants and trees"
-            "Plants and trees"
-            "Plants and trees"
-            "Plants and trees"
-            "Plants and trees",
-            style: TextStyle(
-                color: Colors.white, fontSize: 17, fontWeight: FontWeight.w600)),
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 17,
+              fontWeight: FontWeight.w600
+            )
+          ),
       ),
     );
   }
